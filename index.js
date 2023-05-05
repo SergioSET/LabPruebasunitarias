e70a6b78d17400455d8b82eb0006d0673680a196
Sergio Escudero Tabares <sergio.escudero@correounivalle.edu.co>
2023-05-05 18:24:49 -0500

    Pruebas unitarias

--- index.js
+++ index.js
@@ -30,35 +30,35 @@ function sum(a, b) {
   };
   
   
-//   function complexFunction(a, b, c, d, e, f) {
-//     if (a > 0) {
-//           if (b > 0) {
-//             if (c > 0) {
-//               return "a > 0, b > 0, c > 0";
-//             } else {
-//               return "a > 0, b > 0, c <= 0";
-//             }
-//           } else {
-//             if (c > 0) {
-//               return "a > 0, b <= 0, c > 0";
-//             } else {
-//               return "a > 0, b <= 0, c <= 0";
-//             }
-//           }
-//         } else {
-//           if (d > 0) {
-//             if (e > 0) {
-//               if (f > 0) {
-//                 return "a <= 0, d > 0, e > 0, f > 0";
-//               } else {
-//                 return "a <= 0, d > 0, e > 0, f <= 0";
-//               }
-//             } else {
-//               return "a <= 0, d > 0, e <= 0";
-//             }
-//           } else {
-//             return "a <= 0, d <= 0";
-//           }
-//         }
-//       }
+  function complexFunction(a, b, c, d, e, f) {
+    if (a > 0) {
+          if (b > 0) {
+            if (c > 0) {
+              return "a > 0, b > 0, c > 0";
+            } else {
+              return "a > 0, b > 0, c <= 0";
+            }
+          } else {
+            if (c > 0) {
+              return "a > 0, b <= 0, c > 0";
+            } else {
+              return "a > 0, b <= 0, c <= 0";
+            }
+          }
+        } else {
+          if (d > 0) {
+            if (e > 0) {
+              if (f > 0) {
+                return "a <= 0, d > 0, e > 0, f > 0";
+              } else {
+                return "a <= 0, d > 0, e > 0, f <= 0";
+              }
+            } else {
+              return "a <= 0, d > 0, e <= 0";
+            }
+          } else {
+            return "a <= 0, d <= 0";
+          }
+        }
+      }
       
